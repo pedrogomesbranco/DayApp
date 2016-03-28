24897bd9f71bc0f1baf1c8f8e0528f8316cb9556
Pedro G. Branco <pedrogomesbranco@me.com>
2016-03-28 19:30:06 -0300

    mudei

---
+++ DayApp/DayApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DayApp
+//
+//  Created by Pedro G. Branco on 3/28/16.
+//  Copyright © 2016 Pedro G. Branco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ALELUIA")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
